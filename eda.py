5c1fdaab0d92ff93e11c7ac5a22780cbef0817c3
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2022-06-20 07:15:23 -0500

    load dependencies

---
+++ eda.py
@@ -0,0 +1,12 @@
+#%%
+'''emulate notebooks for code testing'''
+#%%
+# import dependencies
+from glob import glob
+import os
+import glob
+import numpy as np
+import pandas as pd
+# from pandas_profiling import ProfileReport
+import matplotlib.pyplot as plt
+#%%
