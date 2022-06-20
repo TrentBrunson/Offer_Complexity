c54e730a08230d3469f0bb77c0e374af54ff77e8
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2022-06-20 10:23:41 -0500

    loaded US data - no nulls, lots of invalid values

--- eda.py
+++ eda.py
@@ -2,11 +2,25 @@
 '''emulate notebooks for code testing'''
 #%%
 # import dependencies
-from glob import glob
+# from glob import glob
 import os
 import glob
 import numpy as np
 import pandas as pd
 # from pandas_profiling import ProfileReport
 import matplotlib.pyplot as plt
+import seaborn as sns
 #%%
+df = pd.read_csv('data\DOM_NBK_DATA_AMER_202206102000.csv')
+# %%
+df
+# %%
+df.shape
+# %%
+df.columns
+# %%
+# df = df.drop('id', axis=1)
+# not needed
+# %%
+df.describe()
+# %%
