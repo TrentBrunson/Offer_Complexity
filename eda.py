2926e797935a6b2b578df2aeb723c5911ec3304f
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2022-06-21 06:38:17 -0500

    outliers evaluation

--- eda.py
+++ eda.py
@@ -45,6 +45,7 @@ df.info
 df.describe()
 # %%
 df.value_counts()
+df.value_counts().sum() # value counts looked all unique...verifying...true
 # %%
 df.count()
 # %%
@@ -57,7 +58,7 @@ dupes = df.duplicated()
 dupes
 # %%
 sum(dupes)
-# non dupes found in US data set
+# no dupes found in US data set
 # %%
 df.isnull().sum()
 # 0 nulls found
@@ -129,4 +130,57 @@ unique_vals
 # %%
 unique_value_list = list(np.unique(np.array(df_excluded)))
 unique_value_list
+# %%
+'''
+visualize
+'''
+
+fig, ax = plt.subplots(figsize=(10,8))
+sns.boxplot(df_excluded['LOB_ID'],
+                        orient='v'
+                        )
+# %%
+# do all features in one plot
+fig, ax = plt.subplots(figsize=(10,8))
+bp = sns.boxplot(data = df_excluded)
+bp.set_xticklabels(bp.get_xticklabels(), rotation=75)
+# %%
+# scale data for easier comparison 
+# orders of magnitude difference making uninterpreteable
+# get Z-scores
+# first zero mean and unit variance
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaled_array = scaler.fit_transform(df_excluded)
+# %%
+scaled_data = pd.DataFrame(scaled_array, columns=df_excluded.columns)
+scaled_data
+# %%
+scaled_data.describe()
+# %%
+# visaulize scaled data features in one plot
+fig, ax = plt.subplots(figsize=(10,8))
+bp_scaled = sns.boxplot(data = scaled_data)
+bp_scaled.set_xticklabels(bp_scaled.get_xticklabels(), rotation=75)
+# %%
+# interquartile ranges
+Q1 = df_excluded.quantile(0.25)
+Q3 = df_excluded.quantile(0.75)
+
+IQR = Q3 - Q1
+
+print(IQR)
+# %%
+# remove potential outliers technique based on IQR
+# compute D-values not necessary - most of this is categorical data, even though numeric
+outliers_removed_df = df_excluded[~ ((df_excluded < (Q1 - 1.5*IQR)) \
+                                | (df_excluded > (Q3 + 1.5*IQR))).any(axis=1)]
+outliers_removed_df.shape
+# 29% may have no value (27,163)
+# this df kept only 66,721 of 93,884
+# %%
+# do all features in one plot
+fig, ax = plt.subplots(figsize=(10,8))
+bp_outliers_removed = sns.boxplot(data = outliers_removed_df)
+bp.set_xticklabels(bp_outliers_removed.get_xticklabels(), rotation=75)
 # %%
